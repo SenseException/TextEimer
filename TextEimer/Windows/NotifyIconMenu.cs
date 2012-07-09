887c6b46a72ff9d8402a8b58acc54299d7ff7a72
Claudio Zizza <zizza@flagbit.de>
2012-07-09 14:48:09 +0200

    [+FEATURE] menuitem limit handling added, menuitem deletion added

--- TextEimer/Windows/NotifyIconMenu.cs
+++ TextEimer/Windows/NotifyIconMenu.cs
@@ -15,6 +15,7 @@ namespace TextEimer.Windows
         private ContextMenuStrip notifyIconMenu;
         private ToolStripSeparator toolStripSeparator;
         private Dictionary<string, IType> items;
+        private int limit = 4; // TODO: max. items for ContextMenuStrip/Dictionary<string, IType>. Will be later replaced by config value
 
         /// <summary>
         /// Contructor of NotifyIconMenu class.
@@ -27,6 +28,7 @@ namespace TextEimer.Windows
             this.notifyIconMenu = contextMenuStrip;
             this.foregroundWindow = foregroundWindow;
             this.items = new Dictionary<string, IType>();
+            this.notifyIconMenu.KeyUp += DeleteItem_KeyUp;
             this.BuildContextMenuStrip();
         }
 
@@ -44,17 +46,49 @@ namespace TextEimer.Windows
         {
             ToolStripMenuItem clickedItem = (ToolStripMenuItem) sender;
             string key = clickedItem.Name;
-            string value;
 
             if (this.items.ContainsKey(key))
             {
-                value = (string) this.items[key].Value;
-                Clip.SetText(value);
+                this.items[key].AddValueToClipboard();
                 this.foregroundWindow.SetFocusedWindow();
                 this.PasteValue();
             }
         }
 
+        /// <summary>
+        /// deletes an item from the TextEimer item list with the "del" resp "entf" keyboard key
+        /// </summary>
+        /// <param name="sender">sender object of eventhandler</param>
+        /// <param name="e">event aruments of eventhandler</param>
+        private void DeleteItem_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Delete)
+            {
+                foreach (ToolStripItem item in this.notifyIconMenu.Items)
+                {
+                    if (item.Selected)
+                    {
+                        bool itemExists = false;
+                        // check if the item exists in items list
+                        if (this.items.ContainsKey(item.Name))
+                        {
+                            itemExists = this.items.Remove(item.Name);
+                        }
+
+                        // if ToolStripItem is not a Control element then delete ToolStripItem
+                        if (itemExists)
+                        {
+                            this.notifyIconMenu.Items.RemoveByKey(item.Name);
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Paste the clipboard value
+        /// </summary>
         private void PasteValue()
         {
             SendKeys.Send("^v");
@@ -74,14 +108,36 @@ namespace TextEimer.Windows
         /// <summary>
         /// adds a new clipboard value to the items
         /// </summary>
-        /// <param name="typeContainer">a type container</param>
+        /// <param name="typeContainer">a type container with clipboard value</param>
         public void Add(IType typeContainer)
         {
-            this.items.Add(typeContainer.Key, typeContainer);
+            try
+            {
+                // remove item if same typeContainer exists in list
+                if (this.items.ContainsKey(typeContainer.Key))
+                {
+                    this.items.Remove(typeContainer.Key);
+                }
 
-            // TODO: check if the ContextMenuStrip can be build when the menu is called by ContextMenuStrip.Show
-            this.notifyIconMenu.Items.Clear();
-            this.BuildContextMenuStrip();
+                this.items.Add(typeContainer.Key, typeContainer);
+
+                // remove oldest entry if count of items is bigger than the defined limit
+                if (this.items.Count > this.limit)
+                {
+                    KeyValuePair<string, IType> firstItem = this.items.First();
+                    if (this.items.ContainsKey(firstItem.Key))
+                    {
+                        this.items.Remove(firstItem.Key);
+                    }
+                }
+
+                // TODO: check if the ContextMenuStrip can be build when the menu is called by ContextMenuStrip.Show
+                this.BuildContextMenuStrip();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         /// <summary>
@@ -100,27 +156,46 @@ namespace TextEimer.Windows
             ));
         }
 
+        /// <summary>
+        /// builds the ContextMenuStrip with the clipboard value history and the Control Items
+        /// </summary>
         private void BuildContextMenuStrip()
         {
-            int limitCounter = 1;
-            int limit = 20;
-            foreach (KeyValuePair<string, IType> item in this.items)
+            try
             {
-                this.notifyIconMenu.Items.Add(new ToolStripMenuItem(
-                    item.Value.MenuValue,
-                    null,
-                    new EventHandler(Paste_Click),
-                    item.Key
-                ));
+                this.notifyIconMenu.Items.Clear();
 
-                limitCounter++;
-                if (limitCounter >= limit)
+                int limitCounter = 1;
+                ToolStripMenuItem currentItem = null;
+
+                foreach (KeyValuePair<string, IType> item in this.items)
                 {
-                    break;
+                    currentItem = new ToolStripMenuItem(
+                        item.Value.MenuValue,
+                        null,
+                        new EventHandler(Paste_Click),
+                        item.Key
+                    );
+
+                    this.notifyIconMenu.Items.Add(currentItem);
+
+                    limitCounter++;
+                    if (limitCounter >= this.limit)
+                    {
+                        break;
+                    }
+                }
+
+                this.AddControlMenuItems();
+                if (null != currentItem)
+                {
+                    currentItem.Select();
                 }
             }
-
-            this.AddControlMenuItems();
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
