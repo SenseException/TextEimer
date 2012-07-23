dcaad019a88cd21e9b98b0ec7506b445f010bb70
Claudio Zizza <zizza@flagbit.de>
2012-07-23 13:07:23 +0200

    [+FEATURE] settings handling added to options form, logging property and save/cancel buttons added to form

--- TextEimer/Options.Designer.cs
+++ TextEimer/Options.Designer.cs
@@ -31,22 +31,25 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Options));
             this.optionTab = new System.Windows.Forms.TabControl();
             this.settingsTab = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.labelMenuItemAmount = new System.Windows.Forms.Label();
-            this.trackBarMenuItemAmount = new System.Windows.Forms.TrackBar();
+            this.groupBoxLogginOn = new System.Windows.Forms.GroupBox();
+            this.checkBoxLoggingOn = new System.Windows.Forms.CheckBox();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
             this.groupBoxOrderDesc = new System.Windows.Forms.GroupBox();
             this.radioOrderDesc2 = new System.Windows.Forms.RadioButton();
             this.radioOrderDesc1 = new System.Windows.Forms.RadioButton();
+            this.trackBarMenuItemAmount = new System.Windows.Forms.TrackBar();
+            this.labelMenuItemAmount = new System.Windows.Forms.Label();
             this.optionTab.SuspendLayout();
             this.settingsTab.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBarMenuItemAmount)).BeginInit();
+            this.groupBoxLogginOn.SuspendLayout();
             this.groupBoxOrderDesc.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarMenuItemAmount)).BeginInit();
             this.SuspendLayout();
             // 
             // optionTab
             // 
             this.optionTab.Controls.Add(this.settingsTab);
-            this.optionTab.Controls.Add(this.tabPage2);
             this.optionTab.Location = new System.Drawing.Point(1, 1);
             this.optionTab.Name = "optionTab";
             this.optionTab.SelectedIndex = 0;
@@ -55,6 +58,9 @@
             // 
             // settingsTab
             // 
+            this.settingsTab.Controls.Add(this.groupBoxLogginOn);
+            this.settingsTab.Controls.Add(this.buttonSave);
+            this.settingsTab.Controls.Add(this.buttonCancel);
             this.settingsTab.Controls.Add(this.groupBoxOrderDesc);
             this.settingsTab.Controls.Add(this.trackBarMenuItemAmount);
             this.settingsTab.Controls.Add(this.labelMenuItemAmount);
@@ -66,35 +72,46 @@
             this.settingsTab.Text = "Einstellungen";
             this.settingsTab.UseVisualStyleBackColor = true;
             // 
-            // tabPage2
+            // groupBoxLogginOn
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(564, 335);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
-            this.tabPage2.UseVisualStyleBackColor = true;
+            this.groupBoxLogginOn.Controls.Add(this.checkBoxLoggingOn);
+            this.groupBoxLogginOn.Location = new System.Drawing.Point(283, 82);
+            this.groupBoxLogginOn.Name = "groupBoxLogginOn";
+            this.groupBoxLogginOn.Size = new System.Drawing.Size(269, 48);
+            this.groupBoxLogginOn.TabIndex = 8;
+            this.groupBoxLogginOn.TabStop = false;
+            this.groupBoxLogginOn.Text = "Logging";
             // 
-            // labelMenuItemAmount
+            // checkBoxLoggingOn
             // 
-            this.labelMenuItemAmount.AutoSize = true;
-            this.labelMenuItemAmount.Location = new System.Drawing.Point(13, 15);
-            this.labelMenuItemAmount.Name = "labelMenuItemAmount";
-            this.labelMenuItemAmount.Size = new System.Drawing.Size(130, 13);
-            this.labelMenuItemAmount.TabIndex = 0;
-            this.labelMenuItemAmount.Text = "Anzahl der Clipboardwerte";
+            this.checkBoxLoggingOn.AutoSize = true;
+            this.checkBoxLoggingOn.Location = new System.Drawing.Point(6, 18);
+            this.checkBoxLoggingOn.Name = "checkBoxLoggingOn";
+            this.checkBoxLoggingOn.Size = new System.Drawing.Size(138, 17);
+            this.checkBoxLoggingOn.TabIndex = 0;
+            this.checkBoxLoggingOn.Text = "Fehlerlogging aktivieren";
+            this.checkBoxLoggingOn.UseVisualStyleBackColor = true;
+            this.checkBoxLoggingOn.CheckedChanged += new System.EventHandler(this.checkBoxLoggingOn_CheckedChanged);
             // 
-            // trackBarMenuItemAmount
+            // buttonSave
             // 
-            this.trackBarMenuItemAmount.Location = new System.Drawing.Point(16, 31);
-            this.trackBarMenuItemAmount.Maximum = 50;
-            this.trackBarMenuItemAmount.Minimum = 1;
-            this.trackBarMenuItemAmount.Name = "trackBarMenuItemAmount";
-            this.trackBarMenuItemAmount.Size = new System.Drawing.Size(537, 45);
-            this.trackBarMenuItemAmount.TabIndex = 1;
-            this.trackBarMenuItemAmount.TickStyle = System.Windows.Forms.TickStyle.TopLeft;
-            this.trackBarMenuItemAmount.Value = 1;
+            this.buttonSave.Location = new System.Drawing.Point(344, 293);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(101, 32);
+            this.buttonSave.TabIndex = 7;
+            this.buttonSave.Text = "Speichern";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(451, 293);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(101, 32);
+            this.buttonCancel.TabIndex = 6;
+            this.buttonCancel.Text = "Abbrechen";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
             // 
             // groupBoxOrderDesc
             // 
@@ -102,7 +119,7 @@
             this.groupBoxOrderDesc.Controls.Add(this.radioOrderDesc1);
             this.groupBoxOrderDesc.Location = new System.Drawing.Point(16, 82);
             this.groupBoxOrderDesc.Name = "groupBoxOrderDesc";
-            this.groupBoxOrderDesc.Size = new System.Drawing.Size(198, 49);
+            this.groupBoxOrderDesc.Size = new System.Drawing.Size(245, 49);
             this.groupBoxOrderDesc.TabIndex = 5;
             this.groupBoxOrderDesc.TabStop = false;
             this.groupBoxOrderDesc.Text = "Sortierung";
@@ -116,18 +133,39 @@
             this.radioOrderDesc2.TabIndex = 6;
             this.radioOrderDesc2.Text = "Aufsteigend";
             this.radioOrderDesc2.UseVisualStyleBackColor = true;
+            this.radioOrderDesc2.CheckedChanged += new System.EventHandler(this.radioOrderDesc2_CheckedChanged);
             // 
             // radioOrderDesc1
             // 
             this.radioOrderDesc1.AutoSize = true;
-            this.radioOrderDesc1.Checked = true;
             this.radioOrderDesc1.Location = new System.Drawing.Point(6, 19);
             this.radioOrderDesc1.Name = "radioOrderDesc1";
             this.radioOrderDesc1.Size = new System.Drawing.Size(78, 17);
             this.radioOrderDesc1.TabIndex = 5;
-            this.radioOrderDesc1.TabStop = true;
             this.radioOrderDesc1.Text = "Absteigend";
             this.radioOrderDesc1.UseVisualStyleBackColor = true;
+            this.radioOrderDesc1.CheckedChanged += new System.EventHandler(this.radioOrderDesc1_CheckedChanged);
+            // 
+            // trackBarMenuItemAmount
+            // 
+            this.trackBarMenuItemAmount.Location = new System.Drawing.Point(16, 31);
+            this.trackBarMenuItemAmount.Maximum = 50;
+            this.trackBarMenuItemAmount.Minimum = 1;
+            this.trackBarMenuItemAmount.Name = "trackBarMenuItemAmount";
+            this.trackBarMenuItemAmount.Size = new System.Drawing.Size(537, 45);
+            this.trackBarMenuItemAmount.TabIndex = 1;
+            this.trackBarMenuItemAmount.TickStyle = System.Windows.Forms.TickStyle.TopLeft;
+            this.trackBarMenuItemAmount.Value = 1;
+            this.trackBarMenuItemAmount.Scroll += new System.EventHandler(this.trackBarMenuItemAmount_Scroll);
+            // 
+            // labelMenuItemAmount
+            // 
+            this.labelMenuItemAmount.AutoSize = true;
+            this.labelMenuItemAmount.Location = new System.Drawing.Point(13, 15);
+            this.labelMenuItemAmount.Name = "labelMenuItemAmount";
+            this.labelMenuItemAmount.Size = new System.Drawing.Size(130, 13);
+            this.labelMenuItemAmount.TabIndex = 0;
+            this.labelMenuItemAmount.Text = "Anzahl der Clipboardwerte";
             // 
             // Options
             // 
@@ -146,9 +184,11 @@
             this.optionTab.ResumeLayout(false);
             this.settingsTab.ResumeLayout(false);
             this.settingsTab.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBarMenuItemAmount)).EndInit();
+            this.groupBoxLogginOn.ResumeLayout(false);
+            this.groupBoxLogginOn.PerformLayout();
             this.groupBoxOrderDesc.ResumeLayout(false);
             this.groupBoxOrderDesc.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarMenuItemAmount)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -157,11 +197,14 @@
 
         private System.Windows.Forms.TabControl optionTab;
         private System.Windows.Forms.TabPage settingsTab;
-        private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.Label labelMenuItemAmount;
         private System.Windows.Forms.TrackBar trackBarMenuItemAmount;
         private System.Windows.Forms.GroupBox groupBoxOrderDesc;
         private System.Windows.Forms.RadioButton radioOrderDesc2;
         private System.Windows.Forms.RadioButton radioOrderDesc1;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.GroupBox groupBoxLogginOn;
+        private System.Windows.Forms.CheckBox checkBoxLoggingOn;
     }
 }
--- TextEimer/Options.cs
+++ TextEimer/Options.cs
@@ -7,13 +7,118 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
+using TextEimer.Config;
+
 namespace TextEimer
 {
     public partial class Options : Form
     {
-        public Options()
+        Settings settings;
+
+        public Options(Settings settings)
         {
             InitializeComponent();
+            this.settings = settings;
+            this.init();
+        }
+
+        // TODO add validation of config values
+        private void init()
+        {
+            if (this.settings.LoggingOn)
+            {
+                this.checkBoxLoggingOn.Checked = true;
+            }
+            else
+            {
+                this.checkBoxLoggingOn.Checked = false;
+            }
+
+            if (!this.settings.OrderDesc)
+            {
+                this.radioOrderDesc1.Checked = true;
+            }
+            if (this.settings.OrderDesc)
+            {
+                this.radioOrderDesc2.Checked = true;
+            }
+
+            this.trackBarMenuItemAmount.Value = this.settings.MenuItemAmount;
+
+        }
+
+        /// <summary>
+        /// closes the form without saving settings
+        /// </summary>
+        /// <param name="sender">Sender object</param>
+        /// <param name="e">EventArgs object</param>
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        /// <summary>
+        /// saves the settings and closes the form
+        /// </summary>
+        /// <param name="sender">Sender object</param>
+        /// <param name="e">EventArgs object</param>
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            this.settings.Save();
+            this.Close();
+        }
+
+        /// <summary>
+        /// sets the LoggingOn Setting on change
+        /// </summary>
+        /// <param name="sender">Sender object</param>
+        /// <param name="e">EventArgs object</param>
+        private void checkBoxLoggingOn_CheckedChanged(object sender, EventArgs e)
+        {
+            if (this.checkBoxLoggingOn.Checked)
+            {
+                this.settings.LoggingOn = true;
+            }
+            else
+            {
+                this.settings.LoggingOn = false;
+            }
+        }
+
+        /// <summary>
+        /// sets the OrderDesc setting to false
+        /// </summary>
+        /// <param name="sender">Sender object</param>
+        /// <param name="e">EventArgs object</param>
+        private void radioOrderDesc1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (this.radioOrderDesc1.Checked)
+            {
+                this.settings.OrderDesc = false;
+            }
+        }
+
+        /// <summary>
+        /// sets the OrderDesc setting to true
+        /// </summary>
+        /// <param name="sender">Sender object</param>
+        /// <param name="e">EventArgs object</param>
+        private void radioOrderDesc2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (this.radioOrderDesc2.Checked)
+            {
+                this.settings.OrderDesc = true;
+            }
+        }
+
+        /// <summary>
+        /// sets the MenuItemAmount setting to its selected value
+        /// </summary>
+        /// <param name="sender">Sender object</param>
+        /// <param name="e">EventArgs object</param>
+        private void trackBarMenuItemAmount_Scroll(object sender, EventArgs e)
+        {
+            this.settings.MenuItemAmount = this.trackBarMenuItemAmount.Value;
         }
     }
 }
